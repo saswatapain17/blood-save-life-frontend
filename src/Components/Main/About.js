00adc8c7bdfc0987136ae4ee949ad42dd068edeb
Saswata <65461780+saswatapain17@users.noreply.github.com>
2024-08-01 02:35:10 +0530

    Update About.js

--- src/Components/Main/About.js
+++ src/Components/Main/About.js
@@ -5,12 +5,7 @@ const About = () => {
     <div className='px-44'>
       <p className='text-4xl font-bold text-center underline mt-4'>About blood-save-life</p>
       <br /><p className='text-xl'>blood-save-lifeworks as a platform for users to register as blood to either request/donate blood and blood banks to manage their stocks by managing the pending donations and request along with scheduling blood camps and managing them. The system will authenticate the user/bank using their username(mobile) and password to further perform other actions.</p>
-      <br /><p className='text-xl'>It includes managing and tracking blood donations, connecting donors with recipients, and providing real-time information on blood shortages and needs. The platform will include both a user-facing interface and an blood bank’s interface for managing the data.</p>
-      <p className='text-right text-5xl'>
-        <br />
-        <a target="_blank" href="https://github.com/ujjwalSk/" className='hover:drop-shadow-md hover:text-purple'><i class="fa-brands fa-github"></i></a>&nbsp;&nbsp;&nbsp;
-        <a target="_blank" href="https://www.linkedin.com/in/ujjwalsk/" className='hover:drop-shadow-md hover:text-metal'><i class="fa-brands fa-linkedin"></i> </a>&nbsp;&nbsp;
-      </p>
+      <br /><p className='text-xl'>It includes managing and tracking blood donations, connecting donors with recipients, and providing real-time information on blood shortages and needs. The platform will include both a user-facing interface and an blood bank’s interface for managing the data.</p
     </div>
   )
 }
